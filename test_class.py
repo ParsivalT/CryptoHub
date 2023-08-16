5f498b8d186267677f1fbc16bfc0e3318bd11b2e
Thiago Vinicius <tthiagovinnicius@gmail.com>
2023-08-16 23:46:24 +0000

    criando mais testes

--- test_class.py
+++ test_class.py
@@ -1,14 +1,13 @@
 import pytest
 from cryptohub.hub import Coin
+from main import menu
 
 BASE_URL = 'https://min-api.cryptocompare.com/data/price?fsym=COIN&tsyms=FORMAT'
 
 def test_api():
     coin = Coin(base_url=BASE_URL)
+    value, time = coin.check_price('BTC', 'BRL')
 
     assert coin.ping() == True
+    assert value.isdigit() == True
 
-
-
-if __name__ == '__main__':
-    test_api()
